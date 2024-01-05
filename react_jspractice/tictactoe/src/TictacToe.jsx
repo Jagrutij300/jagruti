d432e9ffd1ce656a214b1cd35a7661460b03f401
jagrutij300 <jagrutij300@gmail.com>
2024-01-05 21:36:13 +0530

    alternate coming x and o

--- react_jspractice/tictactoe/src/TictacToe.jsx
+++ react_jspractice/tictactoe/src/TictacToe.jsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import "./Tictactoe.css"
 const Tictactoe = () =>{
     let arr =[".",".",".",".",".",".",".",".",".",]
+    
+    
+
     const [stateArr, setStateArr] = useState(arr);
+    const [player, setplayer] = useState("x")
     return(
 <div style={{display:"grid",gridTemplateColumns:"auto auto auto"}}>
     {stateArr.map(function(ele,ind){
         return <button 
-        onClick={function(){
+            onClick={function(){
             console.log("function working");
-            let temparr = stateArr;
-            temparr[ind]= "x"
-            setStateArr([...temparr])
-        }}
+           
+           stateArr[ind]= player;
+           setplayer(player==="x"?"o":"x")
+            setStateArr([...stateArr])
+       
+       
+       }}
         
         
         >{ele}
