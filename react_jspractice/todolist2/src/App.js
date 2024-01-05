1c0038caf8bcf7cac7e12860d536c65d886578c3
jagrutij300 <jagrutij300@gmail.com>
2024-01-05 18:45:09 +0530

    fatch

--- react_jspractice/todolist2/src/App.js
+++ react_jspractice/todolist2/src/App.js
@@ -1,11 +1,33 @@
  import Todo from "./Components/Todo"
-
+import { useEffect,useState } from "react";
 
 
 function App(){
+ //let arr =["task 1","task 2","task 3"]
+  // normal eriable change in state variable
+let temparr=[]
+
+useEffect(()=>{
+  fetch("https://dummyjson.com/todos")
+  .then(async(res)=>{
+    let temp = await res.json();
+    setArr(temp.todos)
+    // console.log(temparr);
+  })
+  .catch(
+    err=>{
+      console.log(err);
+    }
+  )
+},[]
+
+)
+ 
+
+const [arr , setArr] = useState(temparr)
   return(
-    <div style={{border:"5px solid red", width:"500px",height:"400px", backgroundColor: "pink"  }}>
-    <Todo/>
+    <div style={{border:"5px solid red", width:"500px",height:"400px",  backgroundColor: "pink"  }}>
+    <Todo task={arr} setArr={setArr}/>
     </div>
   )
 }
--- react_jspractice/todolist2/src/Components/Todo.jsx
+++ react_jspractice/todolist2/src/Components/Todo.jsx
@@ -1,10 +1,28 @@
 import React from "react"
 
-const Todo=() =>{
+const Todo=(props) =>{
+
+   let inputval="";
+
     return (
-    <div style={{border:"spx solid blue", backgroundColor:"gray", height:"", width:"300px"}}> 
-   
-    </div>
+    <div style={{border:"spx solid blue",backgroundColor:"gray",display:"flex",justifyContent:"center",alignItems:"center",height:"300px",width:"300px",}}> 
+     
+<input onChange={function(event){
+    inputval = event.target.value
+}}/>
+<button onClick={function(event){
+    props.setArr([...props.task,inputval])
+}}>Add</button>
+<div>
+    {props.task.map(function(ele){
+    return <p style={{marginLeft:"20px",color:"black",fontWeight:"900",fontSize:"20px"}}>
+        {ele.id}
+    {ele.todos}
+    </p>
+    })}
+</div>
+
+    </div>                                                                                                                                          
     )
 }
 export default Todo;
