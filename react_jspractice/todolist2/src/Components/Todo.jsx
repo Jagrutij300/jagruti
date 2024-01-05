48e36c37f6033e23f3f1cb4c188ea26975bb97ff
jagrutij300 <jagrutij300@gmail.com>
2024-01-05 12:32:06 +0530

    new todolist2

---
+++ react_jspractice/todolist2/src/Components/Todo.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Todo=() =>{
+    return (
+    <div style={{border:"spx solid blue", backgroundColor:"gray", height:"", width:"300px"}}> 
+   
+    </div>
+    )
+}
+export default Todo;
