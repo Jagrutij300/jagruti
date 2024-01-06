0c30502dcc2a3aa62047943f9eaee2dee4adb774
jagrutij300 <jagrutij300@gmail.com>
2024-01-06 19:18:13 +0530

    delet

---
+++ react_jspractice/application3/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <h1>class Component Example</h1>
+        <Calculator/>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ react_jspractice/application3/src/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Calculator extends React.Component{
+   
+    render(){
+        return(
+            <div style={{backgroundColor:"pink",fontSize:"32px",}}>
+                Hi I Am A Calculator
+            </div>
+        )
+    }
+}
+export default Calculator
